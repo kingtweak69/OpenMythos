824a827752aeac9734caca572db3a84f27525d73
King Tweak <slimebrazy69@gmail.com>
2026-06-12 13:11:53 -0400

    Remove copyright notice from gdn2.py
    Removed copyright notice from the top of gdn2.py.

--- omni_mythos/gdn2.py
+++ omni_mythos/gdn2.py
@@ -1,12 +1,4 @@
-# Copyright (c) 2026, NVIDIA CORPORATION.  All rights reserved.
-#
-# NVIDIA CORPORATION and its licensors retain all intellectual property
-# and proprietary rights in and to this software, related documentation
-# and any modifications thereto.  Any use, reproduction, disclosure or
-# distribution of this software and related documentation without an express
-# license agreement from NVIDIA CORPORATION is strictly prohibited.
-
-r"""
+"""
 GDN-2 (Gated DeltaNet 2) token-mixing layer.
 
 This module defines `GatedDeltaNet2`, the `nn.Module` that wraps the GDN-2
